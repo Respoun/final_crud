48d2c8c3fb04d9d3259ee4445cb28987c86438fe
Guillaume FITAMANT <Guillaume.FITAMANT@cloud-temple.com>
2020-04-22 14:54:54 +0200

    fix users

--- modules/app/controllers/users.py
+++ modules/app/controllers/users.py
@@ -14,23 +14,23 @@ def user():
     if request.method == 'GET':
         query = request.args
         try:
-            data = mongo.db.Customers.find_one(query)
+            data = mongo.toto.Customers.find_one(query)
             return jsonify(data), 200
         except:
-            return jsonify("je suis une erreur", 500)
+            return jsonify({'ok': False, 'message': 'Database unreachable'}), 500
 
 
     data = request.get_json()
     if request.method == 'POST':
-        if data.get('name', None) is not None and data.get('email', None) is not None:
-            mongo.db.customers.insert_one(data)
+        if data.get('first_name', None) is not None and data.get('email', None) is not None and data.get('id', None) is not None:
+            mongo.db.Customers.insert_one(data)
             return jsonify({'ok': True, 'message': 'User created successfully!'}), 200
         else:
             return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
 
     if request.method == 'DELETE':
         if data.get('email', None) is not None:
-            db_response = mongo.db.customers.delete_one({'email': data['email']})
+            db_response = mongo.db.Customers.delete_one({'email': data['email']})
             if db_response.deleted_count == 1:
                 response = {'ok': True, 'message': 'record deleted'}
             else:
@@ -41,7 +41,7 @@ def user():
 
     if request.method == 'PATCH':
         if data.get('query', {}) != {}:
-            mongo.db.customers.update_one(
+            mongo.db.Customers.update_one(
                 data['query'], {'$set': data.get('payload', {})})
             return jsonify({'ok': True, 'message': 'record updated'}), 200
         else:
