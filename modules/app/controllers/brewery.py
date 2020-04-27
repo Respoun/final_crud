3b16a451a3534775e95c402d6470357fa71aff3e
Guillaume FITAMANT <Guillaume.FITAMANT@cloud-temple.com>
2020-04-27 16:24:47 +0200

    add changes

--- modules/app/controllers/brewery.py
+++ modules/app/controllers/brewery.py
@@ -1,4 +1,4 @@
-''' controller and routes for users '''
+''' controller and routes for brewery '''
 import os
 from flask import request, jsonify
 from app import app, mongo
@@ -9,12 +9,12 @@ LOG = logger.get_root_logger(
     __name__, filename=os.path.join(ROOT_PATH, 'output.log'))
 
 
-@app.route('/user', methods=['GET', 'POST', 'DELETE', 'PATCH'])
-def user():
+@app.route('/brewery', methods=['GET', 'POST', 'DELETE', 'PATCH'])
+def brewery():
     if request.method == 'GET':
         query = request.args
         try:
-            data = mongo.db.Customers.find_one(query)
+            data = mongo.db.Brewery.find_one(query)
             return jsonify(data), 200
         except:
             return jsonify({'ok': False, 'message': 'Database unreachable'}), 500
@@ -22,15 +22,15 @@ def user():
 
     data = request.get_json()
     if request.method == 'POST':
-        if data.get('first_name', None) is not None and data.get('email', None) is not None and data.get('id', None) is not None:
-            mongo.db.Customers.insert_one(data)
-            return jsonify({'ok': True, 'message': 'User created successfully!'}), 200
+        if data.get('id', None) is not None and data.get('id_country', None) is not None and data.get('name', None) is not None:
+            mongo.db.Brewery.insert_one(data)
+            return jsonify({'ok': True, 'message': 'Brewery created successfully!'}), 200
         else:
             return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
 
     if request.method == 'DELETE':
-        if data.get('email', None) is not None:
-            db_response = mongo.db.Customers.delete_one({'id': data['id']})
+        if data.get('id', None) is not None:
+            db_response = mongo.db.Brewery.delete_one({'id': data['id']})
             if db_response.deleted_count == 1:
                 response = {'ok': True, 'message': 'record deleted'}
             else:
@@ -41,7 +41,7 @@ def user():
 
     if request.method == 'PATCH':
         if data.get('query', {}) != {}:
-            mongo.db.Customers.update_one(
+            mongo.db.Brewery.update_one(
                 data['query'], {'$set': data.get('payload', {})})
             return jsonify({'ok': True, 'message': 'record updated'}), 200
         else:
